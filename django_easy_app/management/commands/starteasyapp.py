0200d77c2bd4c45353497888fcb84562fa22a14d
Dwight Hubbard <dhubbard@yahoo-inc.com>
2015-04-12 15:48:44 -0400

    Fix string formatting.

--- django_easy_app/management/commands/starteasyapp.py
+++ django_easy_app/management/commands/starteasyapp.py
@@ -84,7 +84,7 @@ class Command(BaseCommand):
             appname = args[0]
         else:
             appname = options['appname'][0]
-        self.stdout.write('Creating easyapp named: %s', appname)
+        self.stdout.write('Creating easyapp named: %s' % appname)
         call_command('startapp', appname)
         with open(os.path.join(appname, 'urls.py'), 'w') as fh:
             fh.write(basic_urls_py)
