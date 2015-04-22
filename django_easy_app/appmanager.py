64c5cfb0a524f8273107f1cb6bc969dbd1f6df7f
Dwight Hubbard <dhubbard@yahoo-inc.com>
2015-04-22 16:55:59 -0700

    Add a module for updateing the project_ruls

---
+++ django_easy_app/appmanager.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.utils.importlib import import_module
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def updated_app_urlpatterns(urlpatterns):
+    urlpatterns = list(urlpatterns)
+    for app in settings.INSTALLED_APPS:
+        try:
+            _view_module = import_module('%s.views' % app)
+        except ImportError:
+            _view_module = None
+
+        if _view_module and getattr(_view_module, 'easydjango', False):
+            logger.debug('Adding urls for %s module', app)
+            try:
+                _module = import_module('%s.urls' % app)
+                urlpatterns.append(url(r'^%s/' % app, include(_module)))
+            except ImportError:
+                pass
+    return urlpatterns
--- django_easy_app/urlmanager.py
+++ django_easy_app/urlmanager.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.utils.importlib import import_module
 from django.views.generic import View
+
 import inspect
 import logging
 
@@ -69,3 +70,26 @@ def project_urlpatterns(view_module, base_url_path=None):
                         )
                     )
     return view_patterns
+
+
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def updated_app_urlpatterns(urlpatterns):
+    urlpatterns = list(urlpatterns)
+    for app in settings.INSTALLED_APPS:
+        try:
+            _view_module = import_module('%s.views' % app)
+        except ImportError:
+            _view_module = None
+
+        if _view_module and getattr(_view_module, 'easydjango', False):
+            logger.debug('Adding urls for %s module', app)
+            try:
+                _module = import_module('%s.urls' % app)
+                urlpatterns.append(url(r'^%s/' % app, include(_module)))
+            except ImportError:
+                pass
+    return urlpatterns
